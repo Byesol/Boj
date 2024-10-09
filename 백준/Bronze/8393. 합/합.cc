f80dc8bacab422d1e74866e60ce1cac6f7594a81
Byesol <bsj5967@naver.com>
2024-10-10 00:26:10 +0900

    [Bronze V] Title: 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstring> 
+using namespace std;
+
+int main() {
+	int n;
+	//int a, b;
+	cin >> n;
+	int result=0;
+	for (int i = 1; i <= n; i++) {
+		result += i;
+	
+	}
+	cout << result;
+	return 0;
+}
