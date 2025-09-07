21aa95ea60d38b2ced030a4914b11ed9ba73e049
백승주 <bsj5967@naver.com>
2025-09-08 01:49:29 +0900

    [Silver IV] Title: 수 찾기, Time: 544 ms, Memory: 44464 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,57 +1,47 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static List<Integer> numbers = new ArrayList<>();
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] array = new int[n];
         for (int i = 0; i < n; i++) {
-            numbers.add(Integer.parseInt(st.nextToken()));
+            array[i] = Integer.parseInt(st.nextToken());
         }
-        Collections.sort(numbers);
+        Arrays.sort(array);
         int m = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < m; i++) {
-            int find = Integer.parseInt(st.nextToken());
-            int mid = (numbers.size() - 1) / 2;
-            int start = 0;
-            int last = numbers.size() - 1;
-            boolean result = binarySearch(find, start, mid, last);
-            if (result) {
-                sb.append("1\n");
+            int x = Integer.parseInt(st.nextToken());
+            if (binarySearch(array, x)) {
+                sb.append(1).append('\n');
             } else {
-                sb.append("0\n");
+                sb.append(0).append('\n');
             }
         }
         System.out.print(sb);
 
-        br.close();
     }
 
-    private static boolean binarySearch(int find, int start, int mid, int last) {
-        if (find == numbers.get(mid)) {
-            return true;
-        }
-        if (start > last) {
-            return false;
-        }
-        if (find > numbers.get(mid)) {
-            ;
-            int newMid = (mid + last + 1) / 2;
-            return binarySearch(find, mid + 1, newMid, last);
-        }
-        if (find < numbers.get(mid)) {
-            int newMid = (mid + start - 1) / 2;
-            return binarySearch(find, start, newMid, mid - 1);
+    private static boolean binarySearch(int[] array, int target) {
+        int left = 0;
+        int right = array.length - 1;
+
+        while (left <= right) {
+            int middle = (left + right) / 2;
+            if (array[middle] == target) {
+                return true;
+            } else if (array[middle] < target) {
+                left = middle + 1;
+            } else {
+                right = middle - 1;
+            }
         }
         return false;
     }
