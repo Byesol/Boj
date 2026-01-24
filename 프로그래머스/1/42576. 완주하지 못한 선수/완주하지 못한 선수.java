b1314cb3092a4be8111a5b4ac21febd66c3e46e3
백승주 <bsj5967@naver.com>
2026-01-24 19:03:52 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 80.21 ms, Memory: 97.2 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -3,21 +3,24 @@ import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
         Map<String, Integer> map = new HashMap<>();
-        
+
+        // 참가자 수 카운트
         for (String p : participant) {
             map.put(p, map.getOrDefault(p, 0) + 1);
         }
-        
+
+        // 완주자 수 차감
         for (String c : completion) {
             map.put(c, map.get(c) - 1);
         }
-        
+
+        // 남은 사람이 정답
         for (String key : map.keySet()) {
-            if (map.get(key) != 0) {
+            if (map.get(key) > 0) {
                 return key;
             }
         }
-        
-        return null; 
+
+        return "";
     }
 }
