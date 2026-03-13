8e79101db118ec6817566befd508bac7bf3ae659
백승주 <bsj5967@naver.com>
2026-03-13 23:44:28 +0900

    [level 2] Title: 귤 고르기, Time: 71.68 ms, Memory: 119 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -1,6 +1,27 @@
+import java.util.*;
+
 class Solution {
     public int solution(int k, int[] tangerine) {
+        // 크기별 개수 세기
+        Map<Integer, Integer> countMap = new HashMap<>();
+        for (int t : tangerine) {
+            countMap.put(t, countMap.getOrDefault(t, 0) + 1);
+        }
+        
+        // 빈도수 내림차순 정렬
+        List<Integer> counts = new ArrayList<>(countMap.values());
+        counts.sort(Collections.reverseOrder());
+        
         int answer = 0;
+        int total = 0;
+        
+        // 빈도 높은 것부터 k개 채우기
+        for (int count : counts) {
+            answer++;
+            total += count;
+            if (total >= k) break;
+        }
+        
         return answer;
     }
 }
