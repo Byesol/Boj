f476c23d2dd75cd982aec204bdc8f3d06c05f595
백승주 <bsj5967@naver.com>
2026-04-06 23:53:00 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1332 ms, Memory: 122528 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,30 +1,61 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
-import java.util.HashMap;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        HashMap<Integer, Integer> map = new HashMap<>();
 
-        int N = Integer.parseInt(br.readLine());
-        StringTokenizer st1 = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            int temp = Integer.parseInt(st1.nextToken());
-            map.put(temp, map.getOrDefault(temp, 0) + 1);
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++) {
+            array[i] = Integer.parseInt(st.nextToken());
         }
+        Arrays.sort(array);
 
-        int M = Integer.parseInt(br.readLine());
-        StringTokenizer st2 = new StringTokenizer(br.readLine());
-        for (int i = 0; i < M; i++) {
-            int temp = Integer.parseInt(st2.nextToken());
-            sb.append(map.getOrDefault(temp, 0)).append(" ");
+        int m = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < m; i++) {
+            int find = Integer.parseInt(st.nextToken());
+            sb.append(countByBinarySearch(array, find)).append(" ");
         }
+        System.out.println(sb);
+        br.close();
+    }
 
-        System.out.println(sb.toString());
+    private static int countByBinarySearch(int[] array, int find) {
+        return upperBound(array, find) - lowerBound(array, find);
+    }
+
+    private static int lowerBound(int[] array, int target) {
+        int left = 0;
+        int right = array.length;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (array[mid] >= target) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+    private static int upperBound(int[] array, int target) {
+        int left = 0;
+        int right = array.length;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (array[mid] > target) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
     }
 }
